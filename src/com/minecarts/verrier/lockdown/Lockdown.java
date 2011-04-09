8dc390d63f31f54e2a523593972a4d4a83f51133
Kevin <kingersoll@gmail.com>
2011-04-08 21:38:02 -0700

    Look again - the tabs are now spaces!

--- src/com/minecarts/verrier/lockdown/Lockdown.java
+++ src/com/minecarts/verrier/lockdown/Lockdown.java
@@ -207,16 +207,16 @@ public class Lockdown extends JavaPlugin {
     }
     
     public void informPlayer(org.bukkit.entity.Player player){
-    	String playerName = player.getName();
-    	long time = new Date().getTime();
-    	if(!this.msgThrottle.containsKey(playerName)){
-    		this.msgThrottle.put(playerName, new Date());
-    	}
-    	if((time - (this.msgThrottle.get(player.getName()).getTime())) > 1000 * 3){ //every 3 seconds
-    		player.sendMessage(ChatColor.GRAY + "The world is in " + ChatColor.YELLOW + "temporary lockdown mode" + ChatColor.GRAY + " while all plugins are");
-    		player.sendMessage(ChatColor.GRAY + "   properly loaded. Please try again in a few seconds.");
-    		this.msgThrottle.put(player.getName(), new Date());
-    	}
-    		
+        String playerName = player.getName();
+        long time = new Date().getTime();
+        if(!this.msgThrottle.containsKey(playerName)){
+            this.msgThrottle.put(playerName, new Date());
+        }
+        if((time - (this.msgThrottle.get(player.getName()).getTime())) > 1000 * 3){ //every 3 seconds
+            player.sendMessage(ChatColor.GRAY + "The world is in " + ChatColor.YELLOW + "temporary lockdown mode" + ChatColor.GRAY + " while all plugins are");
+            player.sendMessage(ChatColor.GRAY + "   properly loaded. Please try again in a few seconds.");
+            this.msgThrottle.put(player.getName(), new Date());
+        }
+            
     }
 }
